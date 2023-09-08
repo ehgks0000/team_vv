4c35443e154bad5acc605a28b65d8df4297daeb1
서상혁 <ddrrpg@naver.com>
2023-09-08 19:59:19 +0900

    First commit

---
+++ .prettierrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  printWidth: 120,
+  tabWidth: 2,
+  semi: false,
+  singleQuote: true,
+  trailingComma: 'all',
+  jsxBracketSameLine: true,
+  bracketSpacing: true,
+  // importOrder: ['^react$', '<THIRD_PARTY_MODULES>', '^@search/common/(.*)$', '^@/(.*)$', '^../(.*)$', '^./(.*)$'],
+  // importOrderSeparation: true,
+  // importOrderSortSpecifiers: true,
+}
