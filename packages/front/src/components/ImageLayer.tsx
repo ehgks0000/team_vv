6302fb0d2f05273497f2c593542e8196aa9b660d
서상혁 <ddrrpg@naver.com>
2023-10-09 22:19:36 +0900

    html-to-image 이용해서 이미지 만들어서 저장하는 기능 구현

--- packages/front/src/components/ImageLayer.tsx
+++ packages/front/src/components/ImageLayer.tsx
@@ -1,23 +1,48 @@
-import React, { FC } from 'react'
+import React, { FC, useCallback, useRef } from 'react'
 import useImage from 'use-image'
 import { Image, Layer, Stage } from 'react-konva'
 import { useAtomValue } from 'jotai'
 import { hairAtom } from '@/atoms/parts/hair'
+import { toPng } from 'html-to-image'
 
 type Props = {}
 
 const ImageLayer = ({}: Props) => {
   const hair = useAtomValue(hairAtom)
 
+  const ref = useRef<HTMLDivElement>(null)
+
+  const onButtonClick = useCallback(() => {
+    if (ref.current === null) {
+      return
+    }
+
+    toPng(ref.current, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement('a')
+        link.download = 'my-image-name.png'
+        link.href = dataUrl
+        link.click()
+      })
+      .catch((err) => {
+        console.log(err)
+      })
+  }, [ref])
+
   return (
-    <div>
-      <Stage width={window.innerWidth} height={window.innerHeight}>
-        <Layer>
-          <DraggableImage imagePath={'/next.svg'} />
-          <DraggableImage imagePath={hair} />
-        </Layer>
-      </Stage>
-    </div>
+    <>
+      <div ref={ref}>
+        <Stage width={window.innerWidth} height={window.innerHeight}>
+          <Layer>
+            <DraggableImage imagePath={'/next.svg'} />
+            <DraggableImage imagePath={hair} />
+          </Layer>
+        </Stage>
+      </div>
+      <button className="absolute top-0" onClick={onButtonClick}>
+        저장 테스트
+      </button>
+    </>
   )
 }
 
@@ -28,7 +53,7 @@ interface DraggableImageProps {
 const DraggableImage: FC<DraggableImageProps> = ({ imagePath }) => {
   const [image] = useImage(imagePath)
 
-  return <Image image={image} draggable />
+  return <Image image={image} draggable scaleX={1} />
 }
 
 export default ImageLayer
